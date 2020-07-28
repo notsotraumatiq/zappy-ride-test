05a9dce82db29358b9b397646756ce88d8cc552f
notsotraumatiq <atiqpatel95@gmail.com>
2020-07-27 22:19:10 -0400

    skeleton

---
+++ src/Components/TimeDuration/TimeDuration.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import classes from './TimeDuration.module.css'
+
+
+class TimeDuration extends Component {
+    state = {  }
+    render() {
+        return (  );
+    }
+}
+
+export default TimeDuration;
