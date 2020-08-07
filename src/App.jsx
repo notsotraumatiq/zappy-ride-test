8ead8d6664c7e4810bda7611a39d62a30577b745
notsotraumatiq <atiqpatel95@gmail.com>
2020-08-07 00:38:11 -0400

    css

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,32 @@ import Rates from "./Components/Rates/Rates";
 import CalculateResult from "./Components/CalculateResult/CalculateResult";
 import TimeDuration from "./Components/TimeDuration/TimeDuration";
 import MilesPerYear from "./Components/MilesPerYear/MilesPerYear";
+import { css } from "@emotion/core";
+import BeatLoader from "react-spinners/BeatLoader";
 import classes from "./App.module.css";
 
+const override = css`
+  position: fixed;
+  z-index: 999;
+  height: 2em;
+  width: 4em;
+  overflow: visible;
+  margin: auto;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+`;
 class App extends Component {
-  state = {};
+  state = { loading: true };
+
   render() {
     return (
       <>
         <div className={classes.App}>EV CHARGING APP</div>
+        {this.state.loading ? (
+          <BeatLoader css={override} color={"#36D7B7"} />
+        ) : null}
         <Rates />
         <CalculateResult />
       </>
